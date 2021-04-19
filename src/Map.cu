eb3ff55e992cd55fe30345072f12401fe7a01a64
Csehi Ágoston <csehi@sch.bme.hu>
2021-04-19 23:23:25 +0200

    Add project files.

---
+++ src/Map.cu
@@ -0,0 +1,17 @@
+﻿#include "Map.cuh"
+
+__global__ void square(int* dataGPU, int dataSize) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    dataGPU[index] = dataGPU[index] * dataGPU[index];
+}
+
+void map(int* b, const int* a, unsigned int size) {
+    int* dataGPU;
+    cudaMalloc(&dataGPU, sizeof(int) * size);
+    cudaMemcpy(dataGPU, a, sizeof(int) * size, cudaMemcpyHostToDevice);
+    int threadsPerBlock = 256;
+    int blocksPerGrid = 4;
+    square <<<blocksPerGrid, threadsPerBlock >>> (dataGPU, size);
+    cudaMemcpy(b, dataGPU, sizeof(int) * size, cudaMemcpyDeviceToHost);
+    cudaFree(dataGPU);
+}
---
+++ src/Map.cuh
@@ -0,0 +1,9 @@
+﻿#ifndef _MAP_
+#define _MAP_
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+	void map(int* b, const int* a, unsigned int size);
+
+#endif
